1564e477faf02e0ad4ddb4c0b8945ec571057359
Antoine Luciani <antoine1fr@gmail.com>
2017-05-05 11:58:54 +0200

    Reuse Traversable module in OBResult

--- lib/OBResult.ml
+++ lib/OBResult.ml
@@ -12,14 +12,13 @@ module type S = sig
 
   include module type of Monad.Core
   include module type of Applicative.Core
+  include OBTraversable.S2
+    with type ('a, 'b) t := ('a, 'b) t
 
   val choose:
     ('ok, 'err Accu.t) t
     -> ('ok, 'err Accu.t) t
     -> ('ok, 'err Accu.t) t
-
-  val traverse: ('a -> ('b, 'err) t) -> 'a list -> ('b list, 'err) t
-  val sequence: ('ok, 'err) t list -> ('ok list, 'err) t
 
   module Infix: sig
     include module type of Monad.Infix
@@ -58,22 +57,13 @@ module Make(Accu: OBMonoid.S): S
   module Applicative = OBApplicative.Make2(Kernel)
   include Applicative.Core
 
+  include OBTraversable.Make2(Monad)
+
   let choose lhs rhs =
     match lhs, rhs with
     | Ok _, _ -> lhs
     | _, Ok _ -> rhs
     | Error a, Error b -> Error (Accu.add a b)
-
-  let rec traverse_ f accu l =
-    match l with
-    | [] -> accu |> List.rev |> return
-    | head :: tail ->
-      bind (f head) (fun x ->
-        traverse_ f (x :: accu) tail)
-
-  let traverse f l = traverse_ f [] l
-
-  let sequence l = traverse (fun x -> x) l
 
   module Infix = struct
     include Monad.Infix
